58a3ceb0911406c57235500a207e10efb22a346f
Lúcás Meier <cronokirby@gmail.com>
2017-10-18 21:30:34 +0200

    better formatting

--- src/Data/Currency.hs
+++ src/Data/Currency.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
 {-|
 Module : Data.Currency
 Description : Simple module for formatting currency
@@ -8,26 +10,79 @@ module Data.Currency
       -- These types can and should be generated automatically from a locale / currency
       Placement (..)
     , Format (..)
+      -- * Formatting functions
+    , formatNum
     ) where
 
-import Data.Text (Text)
-import Data.Word (Word64)
+
+import Data.Fixed (mod')
+import qualified Data.Text as T
 
 
 -- | Used to indicate where to place one element with regards to another.
 -- For example, to place the $ sign before or after the number
 data Placement
-    = Front -- ^ In the front
-    | Back -- ^ In the back
+    = Before -- ^ Before the element
+    | After -- ^ After the element
 
 
 -- | Represents the formatting options for a currency
 data Format = Format 
-    { displayDecimals :: Word64 -- ^ How many decimals to display? (>= 0)
-    , decimalSep :: Char  -- ^ What character to seperate decimals with?
-    , groupSep :: Char -- ^ What character to seperate groups to the left of the decimal with?
-    , groupSize :: Word64 -- ^ How many characters to group by? (> 0)
-    , symbol :: Text -- ^ What symbol represents this currency?
+    { displayDecimals :: Int -- ^ How many decimals to display? (>= 0)
+    , decimalSep :: T.Text  -- ^ The seperator for the decimals
+    , groupSep :: T.Text -- ^ The seperator for groups left of the decimal
+    , groupSize :: Int -- ^ How many characters to group by? (> 0)
+    , symbol :: T.Text -- ^ What symbol represents this currency?
     , symbolPlacement :: Placement -- ^ Where to place this symbol?
-    , negativePlacement :: Placement -- ^ Where to place the negative symbol (-)?
+    , negativePlacement :: Placement -- ^ Where to place the negative symbol (-) relative to the number?
+    , negativeToSymbol :: Placement -- ^ Place the negative symbol before or after placing the currency? (irrelevant if on different sides)
     }
+
+
+-- Utility function to split from end
+splitAtEnd :: Int -> T.Text -> (T.Text, T.Text)
+splitAtEnd n t = (T.dropEnd n t, T.takeEnd n t)
+
+chunksOfEnd :: Int -> T.Text -> [T.Text]
+chunksOfEnd k = reverse . go
+  where
+    go t = case splitAtEnd k t of
+            (a, b) | T.null a -> [b]
+                   | otherwise -> b : go a
+
+
+-- Turns a placement into a function for building text
+place :: Placement -> T.Text -> T.Text -> T.Text
+place Before = T.append
+place After = flip T.append
+
+
+-- Given a format, and a number converts the number to a currency 
+-- this assumes the decimals have already been rounded
+formatWith :: Format -> Bool -> T.Text -> T.Text -> T.Text
+formatWith Format{..} isNegative front decimals = 
+    let frontChunks = chunksOfEnd groupSize front
+        seppedFront = T.intercalate groupSep frontChunks
+        body = if decimals == ""
+            then seppedFront
+            else T.concat [seppedFront, decimalSep, decimals]
+        placeSymbol = place symbolPlacement symbol
+        placeNeg = if isNegative then place negativePlacement "-" else id
+    in case negativeToSymbol of
+        Before ->
+            placeSymbol (placeNeg body)
+        After ->
+            placeNeg (placeSymbol body)
+
+
+formatNum :: RealFrac n => Format -> n -> T.Text
+formatNum f num = formatWith f isNegative notDecimals nonEmptyDecimals
+  where
+    corrected = num * signum num
+    isNegative = negate num == abs num
+    toText = T.pack . show
+    notDecimals = toText (floor corrected)
+    nDecimals = round ((10 ^ displayDecimals f) * mod' corrected 1)
+    nonEmptyDecimals = if nDecimals == 0
+        then ""
+        else toText nDecimals
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,22 @@
+{-# LANGUAGE OverloadedStrings #-}
+import Test.Hspec
+
+import Data.Currency
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+    formatWithTest
+
+
+formatWithTest = describe "formatNum" $ do
+    it "parses a simple format" $ 
+        formatNum format 1200300.106 == "$1,200,300.11"
+    it "parses negatives" $
+        formatNum format (-1200300.106) == "-$1,200,300.11"
+    it "parses different orders" $
+        formatNum (format {negativeToSymbol = Before}) (-1200.189) == "$-1,200.19"
+    it "formats without decimals" $
+        formatNum format 1200 == "$1,200"
+  where
+    format = Format 2 "." "," 3 "$" Before Before After
+
